6c776c2594a0bee3c2493ac90e85556d6abf5a90
Mike Miles <mikemiles19@gmail.com>
2021-06-07 11:11:51 -0600

    first attempt, builds but segfaults on conversion from pcl msg to pcl, not sure why

--- src/grid_map_node.cpp
+++ src/grid_map_node.cpp
@@ -6,35 +6,63 @@
 #include "grid_map_pcl/GridMapPclLoader.hpp"
 #include "grid_map_pcl/helpers.hpp"
 
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+
 namespace gm = ::grid_map::grid_map_pcl;
 
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "grid_map_pcl_loader_node");
-  ros::NodeHandle nh("~");
-  gm::setVerbosityLevelToDebugIfFlagSet(nh);
-
-  ros::Publisher gridMapPub;
-  gridMapPub = nh.advertise<grid_map_msgs::GridMap>("grid_map_from_raw_pointcloud", 1, true);
-
+class GridMapNode {
+public:
   grid_map::GridMapPclLoader gridMapPclLoader;
-  const std::string pathToCloud = gm::getPcdFilePath(nh);
-  gridMapPclLoader.loadParameters(gm::getParameterPath());
-  gridMapPclLoader.loadCloudFromPcdFile(pathToCloud);
+  bool cloudUpdated;
+  ros::Subscriber inputPub;
+  ros::Publisher outputPub;
+  ros::Timer processTimer;
+  ros::NodeHandle nh;
 
-  gm::processPointcloud(&gridMapPclLoader, nh);
+public:
+  GridMapNode(ros::NodeHandle& nodeh) : nh(nodeh)
+  {
+    cloudUpdated=false;
+    inputPub = nh.subscribe("input", 1, &GridMapNode::callback, this);
+    outputPub = nh.advertise<sensor_msgs::PointCloud2>("output", 1);
+    processTimer = nh.createTimer(ros::Duration(1.0), &GridMapNode::loop, this);
+  }
 
-  grid_map::GridMap gridMap = gridMapPclLoader.getGridMap();
-  gridMap.setFrameId(gm::getMapFrame(nh));
+  inline void callback(const sensor_msgs::PointCloud2& input_msg)
+  {
+    ROS_INFO("Got cloud!");
+    cloudUpdated=true;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr input_cloud;
+    pcl::fromROSMsg(input_msg, *input_cloud);
+    gridMapPclLoader.setInputCloud(input_cloud);
+    ROS_INFO("Done w cloud");
+  }
 
-  gm::saveGridMap(gridMap, nh, gm::getMapRosbagTopic(nh));
+  inline void loop(const ros::TimerEvent& event) {
+    if (cloudUpdated) {
+      ROS_INFO("Publishing cloud!");
+      cloudUpdated=false;
+      gm::processPointcloud(&gridMapPclLoader, nh);
+      // grid_map_msgs::GridMap msg;
+      grid_map::GridMap gridMap = gridMapPclLoader.getGridMap();
+      gridMap.setFrameId(gm::getMapFrame(nh));
+      sensor_msgs::PointCloud2 output_msg;
+      grid_map::GridMapRosConverter::toPointCloud(gridMap, "flat", output_msg);
+      outputPub.publish(output_msg);
+      ROS_INFO("Done w publishing");
+    }
+  }
+};
 
-  // publish grid map
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "grid_map_node");
+  ros::NodeHandle nodeh("~");
+  // gm::setVerbosityLevelToDebugIfFlagSet(nh);
 
-  grid_map_msgs::GridMap msg;
-  grid_map::GridMapRosConverter::toMessage(gridMap, msg);
-  gridMapPub.publish(msg);
+  GridMapNode gmn(nodeh);
 
-  // run
+  ROS_INFO("Initialized.");
+
   ros::spin();
-  return EXIT_SUCCESS;
 }
